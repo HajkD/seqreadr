f43ef9e859c0cde7be87c31d30d1b5f490bd49ee
HajkD <hajkdrost@googlemail.com>
2015-09-24 18:23:58 +0100

    now read.proteome() is based on dplyr's data_frame() methodology instead of a data.table representation

--- R/read.proteome.R
+++ R/read.proteome.R
@@ -1,24 +1,25 @@
 #' @title Read the proteome of a given organism
-#' @description This function reads an organism specific proteome stored in a defined file format.
+#' @description Read an organism specific proteome stored in a fasta/fastq file format.
 #' @param file a character string specifying the path to the file storing the proteome.
-#' @param format a character string specifying the file format used to store the proteome, e.g. "fasta", "fastq".
+#' @param format a character string specifying the file format used to store the proteome, e.g. \code{format = "fasta"} or \code{format = "fastq"}.
 #' @param ... additional arguments that are used by the \code{\link[Biostrings]{readAAStringSet}} function.
 #' @author Hajk-Georg Drost
-#' @details The \code{read.proteome} function takes a string specifying the path to the proteome file
+#' @details This function takes a string specifying the path to the proteome file
 #' of interest as first argument.
 #'
 #' It is possible to read in different proteome file standards such as \emph{fasta} or \emph{fastq}.
 #'
-#' Proteomes stored in fasta files can be downloaded from http://www.ebi.ac.uk/reference_proteomes.
+#' Proteomes stored in fasta format can be downloaded from http://www.ebi.ac.uk/reference_proteomes.
 #'
-#' @examples \dontrun{
+#' @examples 
 #' # reading a proteome stored in a fasta file
-#' Ath.proteome <- read.proteome(system.file('seqs/ortho_thal_aa.fasta', package = 'orthologr'),
+#' Ath.proteome <- read.proteome(system.file('seqs/ortho_thal_aa.fasta', package = 'seqreadr'),
 #'                                format = "fasta")
-#' }
-#'
-#' @return A data.table storing the gene id in the first column and the corresponding
-#' sequence as string in the second column.
+#' 
+#' dplyr::glimpse(Ath.proteome)
+#' 
+#' @return 
+#' A \code{\link{data.frame}} storing the \code{gene id} in the first column, the corresponding sequence as string in the second column, and the sequence length in the third column.
 #' @export
 
 read.proteome <- function(file, format, ...){
@@ -28,19 +29,19 @@ read.proteome <- function(file, format, ...){
   
   geneids <- seqs <- NULL 
   
-  
   tryCatch({
     
     proteome <- Biostrings::readAAStringSet(filepath = file, format = format, ...)
     proteome_names <- as.vector(unlist(sapply(proteome@ranges@NAMES, function(x){return(strsplit(x, " ")[[1]][1])})))
-    proteome.dt <- data.table::data.table(geneids = proteome_names,
-                                          seqs = toupper(as.character(proteome)))
     
-    data.table::setkey(proteome.dt, geneids)
+    proteome.df <- dplyr::data_frame(geneids = proteome_names,
+                                     seqs = toupper(as.character(proteome)))
+    
+    proteome.df <- dplyr::mutate(proteome.df, seqLen = stringr::str_length(seqs))
     
   }, error = function(e){ stop("File ",file, " could not be read properly.", "\n",
-                               "Please make sure that ",file," contains only amino acid sequences and is in ",format," format.")}
+                               "Please make sure that ",file," contains only amino acid sequences and is in ",format," format.", call. = FALSE)}
   )
   
-  return(proteome.dt)
+  return(proteome.df)
 }
