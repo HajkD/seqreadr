e24594498e211829075bdaff2603c7bf2af18661
HajkD <hajkdrost@googlemail.com>
2015-09-24 18:35:28 +0100

    now read.genome() is based on dplyr's data_frame() methodology instead of a data.table representation

--- R/read.genome.R
+++ R/read.genome.R
@@ -1,23 +1,22 @@
 #' @title Read the genome of a given organism
-#' @description This function reads an organism specific genome stored in a defined file format.
+#' @description Read an organism specific genome stored in fasta/fastq file format.
 #' @param file a character string specifying the path to the file storing the genome.
-#' @param format a character string specifying the file format used to store the genome, e.g. "fasta", "fastq".
+#' @param format a character string specifying the file format used to store the genome, e.g. \code{format = "fasta"} or \code{format = "fastq"}.
 #' @param ... additional arguments that are used by the \code{\link[Biostrings]{readDNAStringSet}} function.
-#' @author Hajk-Georg Drost and Sarah Scharfenberg
+#' @author Hajk-Georg Drost
 #' @details The \code{read.genome} function takes a string specifying the path to the genome file
 #' of interest as first argument.
 #'
-#' It is possible to read in different genome file standards such as \emph{fasta} or \emph{fastq}.
 #' Genomes stored in fasta files can be downloaded from http://ensemblgenomes.org/info/genomes.
 #'
-#' @examples \dontrun{
+#' @examples 
 #' # reading a genome stored in a fasta file
-#' Ath.genome <- read.genome(system.file('seqs/ortho_thal_cds.fasta', package = 'orthologr'),
+#' Ath.genome <- read.genome(system.file('seqs/ortho_thal_cds.fasta', package = 'seqreadr'),
 #'                            format = "fasta")
-#' }
 #'
-#' @return A data.table storing the gene id in the first column and the corresponding
-#' sequence as string in the second column.
+#' dplyr::glimpse(Ath.genome)
+#'
+#' @return A \code{\link{data.frame}} storing the \code{gene id} in the first column, the corresponding sequence as string in the second column, and the sequence length in the third column.
 #' @export
 
 read.genome <- function(file, format, ...){
@@ -33,15 +32,16 @@ read.genome <- function(file, format, ...){
       
       genome <- Biostrings::readDNAStringSet(filepath = file, format = format, ...)
       genome_names <- as.vector(unlist(sapply(genome@ranges@NAMES, function(x){return(strsplit(x, " ")[[1]][1])})))
-      genome.dt <- data.table::data.table(geneids = genome_names,
-                                          seqs = toupper(as.character(genome)))
       
-      data.table::setkey(genome.dt,geneids)
+      genome.df <- dplyr::data_frame(geneids = genome_names,
+                                     seqs = toupper(as.character(genome)))
+      
+      genome.df <- dplyr::mutate(genome.df, seqLen = stringr::str_length(seqs))
       
     }, error = function(e){ stop(paste0("File ",file, " could not be read properly. \n",
-                                        "Please make sure that ",file," contains only DNA sequences and is in ",format," format."))}
+                                        "Please make sure that ",file," contains only DNA sequences and is in ",format," format."), call. = FALSE)}
     )
   }
   
-  return(genome.dt)
+  return(genome.df)
 }
